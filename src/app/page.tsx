be98586ec644bfcf2dcfdb5970f59fe7d54780a4
KUNZIT <vdmk1@live.com>
2025-02-28 11:03:13 +0100

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from 'react';
 import { useAccount, useConnect, useDisconnect, useEnsName } from 'wagmi';
 import { EnsDisplay } from './EnsDisplay';
-
+import { useState } from 'react';
 
 function App() {
   const account = useAccount();
