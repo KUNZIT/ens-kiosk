45b927bdbc7f21f364f71d012c57a00b68ace01d
KUNZIT <vdmk1@live.com>
2025-03-01 18:22:29 +0100

    =

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,7 +1,6 @@
 import { configureChains, createConfig } from 'wagmi';
 import { mainnet, sepolia } from 'wagmi/chains';
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
+import { WalletConnectConnector } from '@wagmi/connectors/walletConnect'; // Import from correct path
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, publicClient } = configureChains(
@@ -12,7 +11,6 @@ const { chains, publicClient } = configureChains(
 export const config = createConfig({
   autoConnect: true,
   connectors: [
-    new MetaMaskConnector({ chains }),
     new WalletConnectConnector({
       chains,
       projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID || '', // Ensure projectId is provided
