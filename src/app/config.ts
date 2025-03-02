f5d2939eb40916685c29674b71c5afc349853673
KUNZIT <vdmk1@live.com>
2025-03-02 09:15:13 +0100

    e

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,10 +1,27 @@
-import { http, createConfig } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
+import { configureChains, createConfig } from 'wagmi';
+import { mainnet, sepolia } from 'wagmi/chains';
+import { WalletConnectConnector } from '@wagmi/connectors/walletConnect';
+import { alchemyProvider } from '@wagmi/core/providers/alchemy'; // Correct import path
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, sepolia],
+  [
+    alchemyProvider({
+      apiKey: process.env.ALCHEMY_API_KEY || '',
+    }),
+  ]
+);
 
 export const config = createConfig({
-  chains: [mainnet, sepolia],
-  transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
-  },
-})
+  autoConnect: true,
+  connectors: [
+    new WalletConnectConnector({
+      chains,
+      projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID ?? '',
+    }),
+  ],
+  publicClient,
+  webSocketPublicClient,
+});
+
+export { chains };
