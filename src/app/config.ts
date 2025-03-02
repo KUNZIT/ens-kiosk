6a560d7316d4b0b3b558cd18a2ce47a9225268d3
KUNZIT <vdmk1@live.com>
2025-03-02 14:39:13 +0100

    w

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,10 +2,7 @@ import { createConfig } from 'wagmi';
 import { mainnet, sepolia } from 'wagmi/chains';
 import { walletConnect } from 'wagmi/connectors';
 
-const {} =(
-  [mainnet, sepolia],
-  [] // Removed alchemy provider temporarily
-);
+
 
 export const config = createConfig({
   autoConnect: true,
