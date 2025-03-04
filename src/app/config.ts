199dd845c6346ce0b13fe670adb25636659c8dbc
KUNZIT <vdmk1@live.com>
2025-03-04 13:06:53 +0100

    y

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,12 @@
 import { http, createConfig } from 'wagmi'
 import { mainnet, sepolia } from 'wagmi/chains'
 import { walletConnect } from 'wagmi/connectors'
-const projectId = process.env.WALLETCONNECT_PROJECT_ID;
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
+
+
+if (!projectId) {
+  throw new Error("NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID is not defined in environment variables.");
+}
 
 export const config = createConfig({
   chains: [mainnet, sepolia],
