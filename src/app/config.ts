63493f40411737c1d3b9eb9c8b449da66e72ecff
KUNZIT <vdmk1@live.com>
2025-03-04 08:26:12 +0100

    s

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,10 +1,18 @@
 import { http, createConfig } from 'wagmi'
 import { mainnet, sepolia } from 'wagmi/chains'
 
+const projectId = '<WALLETCONNECT_PROJECT_ID>'
+
 export const config = createConfig({
-  chains: [mainnet, sepolia],
+  chains: [mainnet, sepola],
+  connectors: [
+    
+    walletConnect({ projectId }),
+    
+    
+  ],
   transports: {
     [mainnet.id]: http(),
-    [sepolia.id]: http(),
+    [base.id]: http(),
   },
 })
