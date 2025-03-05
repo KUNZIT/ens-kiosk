0cb2ff460c5654151e3de90a13b36783cc45ff0b
KUNZIT <vdmk1@live.com>
2025-03-05 11:44:21 +0100

    s

--- src/app/WhitelistedModal.js
+++ src/app/WhitelistedModal.js
@@ -8,17 +8,17 @@ const WhitelistedModal = ({ message }) => {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        backgroundColor: 'white',
+        backgroundColor: 'black',
         padding: '20px',
         borderRadius: '8px',
         boxShadow: '0 4px 8px rgba(0, 0, 0, 0.5)',
         zIndex: 1001,
         color: 'black', // Changed to black for better contrast
         textAlign: 'center',
-        border: '1px solid green', // Added green border
+        border: '1px solid white', // Added green border
       }}
     >
-      <p style={{ fontSize: '1.2em', color: 'green' }}>{message}</p>
+      <p style={{ fontSize: '1.2em', color: 'white' }}>{message}</p>
     </div>
   );
 };
