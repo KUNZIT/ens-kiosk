666444e377f501723b790c72320965ae07b5ac8c
Vadims Kokins <94623385+KUNZIT@users.noreply.github.com>
2025-03-17 20:24:24 +0100

    Update efpUtils.ts

--- src/app/efpUtils.ts
+++ src/app/efpUtils.ts
@@ -1,17 +1,14 @@
-
-
 export async function getGradoFollowing(): Promise<any> {
   try {
     const response = await fetch(`https://api.ethfollow.xyz/api/v1/users/grado.eth/following?limit=2000`);
     const data = await response.json();
-    return data.following; // Assuming the API response structure
+    return data.following;
   } catch (error) {
-    console.error('Error fetching grado.eth following list:', error);
     throw error;
   }
 }
 
-export interface Follow { // Add "export" here
+export interface Follow {
   efp_list_nft_token_id: string;
   address: string;
   tags: string;
@@ -26,7 +23,6 @@ export async function isUserFollowedByGrado(userAddress: string): Promise<boolea
     const following = await getGradoFollowing();
     return following.some((follow: Follow) => follow.address.toLowerCase() === userAddress.toLowerCase());
   } catch (error) {
-    console.error('Error checking if user is followed by grado.eth:', error);
     return false;
   }
 }
