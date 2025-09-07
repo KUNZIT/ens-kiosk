7f20b0f777b6589c474c0adf02764ce79d5f50c9
Vadims Kokins <94623385+KUNZIT@users.noreply.github.com>
2025-09-07 21:44:41 +0200

    Update web-serial.d.ts

--- web-serial.d.ts
+++ web-serial.d.ts
@@ -1,3 +1,26 @@
+interface SerialPortInfo {
+  usbVendorId?: number;
+  usbProductId?: number;
+}
+
+interface SerialPort {
+  readonly readable: ReadableStream<Uint8Array> | null;
+  readonly writable: WritableStream<Uint8Array> | null;
+  open(options: { baudRate: number }): Promise<void>;
+  close(): Promise<void>;
+  getInfo(): SerialPortInfo;
+  forget(): Promise<void>;
+}
+
+interface Serial extends EventTarget {
+  onconnect: ((this: this, ev: Event) => any) | null;
+  ondisconnect: ((this: this, ev: Event) => any) | null;
+  getPorts(): Promise<SerialPort[]>;
+  requestPort(options?: { filters: any[] }): Promise<SerialPort>;
+  addEventListener(type: "connect" | "disconnect", listener: (this: this, ev: Event) => any, options?: boolean | AddEventListenerOptions): void;
+  removeEventListener(type: "connect" | "disconnect", listener: (this: this, ev: Event) => any, options?: boolean | EventListenerOptions): void;
+}
+
 interface Navigator {
-  serial: any; // You can use `any` for simplicity or define the full `Serial` type
+  serial: Serial;
 }
